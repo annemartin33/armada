2299ca676e464657671202f64f77700e90fb7f73
Raghu Betina <raghu@firstdraft.com>
2019-08-27 03:01:33 +0000

    Whitelist columns in Traveler for ActiveAdmin

--- app/admin/travelers.rb
+++ app/admin/travelers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Traveler do
+
+ permit_params :email, :password, :home_airport, :username
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
