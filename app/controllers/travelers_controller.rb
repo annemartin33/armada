f9c5dec49b51a67e030679a5bcf699b639bbeab1
Raghu Betina <raghu@firstdraft.com>
2019-08-27 03:00:31 +0000

    Generate travelers resource with devise

---
+++ app/controllers/travelers_controller.rb
@@ -0,0 +1,13 @@
+class TravelersController < ApplicationController
+  def index
+    @travelers = Traveler.all
+
+    render("traveler_templates/index.html.erb")
+  end
+
+  def show
+    @traveler = Traveler.find(params.fetch("id_to_display"))
+
+    render("traveler_templates/show.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :travelers
+  # Routes for the Traveler resource:
+
+  # READ
+  get("/travelers", { :controller => "travelers", :action => "index" })
+  get("/travelers/:id_to_display", { :controller => "travelers", :action => "show" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
